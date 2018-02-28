032732d5604420d0d8ff882aba98b4eee9cc0e8e
Unknown <swilsondev@outlook.com>
2018-02-28 08:39:16 -0600

    Setting up db.php.

--- includes/db.php
+++ includes/db.php
@@ -14,3 +14,33 @@
 // Make sure to include support for DB_PORT. See the
 // PHP Documentation for the MySQL PDO DSN:
 // http://php.net/manual/en/ref.pdo-mysql.connection.php
+
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$dbname = getenv("DB_DATABASE");
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+$user = getenv("DB_USERNAME");
+$pswd = getenv("DB_PASSWORD");
+$conn =  new PDO($dsn, $user, $pswd);
+
+function GetMany($query, $conn, $toBind = []) {
+    $statement = $conn->prepare($query);
+    foreach($toBind as $bString => $bValue) {
+        $statement->bindValue($bString, $bValue);        
+    }
+    $statement->execute();
+    $toReturn = $statement->fetchAll();
+    $statement->closeCursor();
+    return $toReturn;
+}
+
+function GetOne($query, $conn, $toBind = []) {
+    $statement = $conn->prepare($query);
+    foreach($toBind as $bString => $bValue) {
+        $statement->bindValue($bString, $bValue);        
+    }
+    $statement->execute();
+    $toReturn = $statement->fetch();
+    $statement->closeCursor();
+    return $toReturn;
+}
--- index.php
+++ index.php
@@ -5,13 +5,10 @@
 // error, we will show you exactly what went wrong!
 // This line should be on every page you create.
 require_once('./includes/init.php');
+require_once('./includes/db.php');
 
 // Here you might connect to the database and show off some of your newest guitars.
 
+var_dump(GetMany("SELECT * FROM categories", $conn));
 ?>
 
-This is my super awesome Guitar Shop!
-
-- All Orders
-- Unshipped Orders
-- Products
