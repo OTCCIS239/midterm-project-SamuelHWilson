feab12bde13bcb5c5a4a1c44dd7b7561adff37ad
Unknown <swilsondev@outlook.com>
2018-03-01 17:00:48 -0600

    Setting up Vuetify template.

---
+++ code.js
@@ -0,0 +1,18 @@
+Vue.component('page', {
+    template: '#page'
+})
+new Vue({
+    el: '#app',
+    data: {
+        clipped: true,
+        drawer: true,
+        fixed: false,
+        items: [
+            { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+    }
+})
